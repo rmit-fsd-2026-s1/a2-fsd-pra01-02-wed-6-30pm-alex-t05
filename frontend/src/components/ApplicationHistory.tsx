abd66af71e576e2d25fda507fc65901262a86bee
s3731804 <s3731804@student.rmit.edu.au>
2026-05-12 15:23:30 +1000

    refactor: remove console logs from ApplicationHistory component

--- frontend/src/components/ApplicationHistory.tsx
+++ frontend/src/components/ApplicationHistory.tsx
@@ -4,16 +4,7 @@ import { useEvent } from "@/context/EventContext";
 import { useEffect, useState } from "react";
 export default function ApplicationHistory({application}: {application: Application}) {
     const { events } = useEvent();
-    console.log(events);
-    console.log("Application in history component:", application);
-    console.log("Application event ID:", application.eventId);
-    for (const event of events) {
-        if (event.eventId === application.eventId) {
-            console.log("Matching event found:", event);
-        }
-    }
     const eventByApplication = events.find(event => event.eventId === application.eventId);
-    console.log("Event by application:", eventByApplication);
     return (
         <Box>
             {eventByApplication ? eventByApplication.eventName : "Unknown Event"}: {application.startDate} to {application.endDate.slice(5)} - Status: {application.status} {application.rating ? `- Rating: ${application.rating}/5` : ""}
