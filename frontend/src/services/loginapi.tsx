1277a8a160c3e969a2c43bb4645583078257730f
s3731804 <s3731804@student.rmit.edu.au>
2026-06-04 19:40:33 +1000

    fix to auth context handling the new log in system

--- frontend/src/services/loginapi.tsx
+++ frontend/src/services/loginapi.tsx
@@ -7,7 +7,7 @@ const API_BASE_URL = "http://localhost:3001/api";
 // Will need to add validations in the future.
 
 export const authService = {
-    loginUser: async (email: string, password: string): Promise<void> => {
+    loginUser: async (email: string, password: string): Promise<User> => {
         const { data } = await axios.post(`${API_BASE_URL}/login`, { email, password });
         return data;
     }
