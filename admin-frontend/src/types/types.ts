e6437f76695a410c242cdc07408967b2d5c8a96a
Minh Nguyen <s3989825@student.rmit.edu.au>
2026-06-06 20:41:17 +1000

    trying to reuse lectorial 9 for this assessment for graphql

---
+++ admin-frontend/src/types/types.ts
@@ -0,0 +1,25 @@
+export interface Admin {
+    userName: string;
+    password: string;
+}
+
+export interface Event {
+    eventId: number;
+    eventName: string;
+    numberOfGuest: number;
+    address: string;
+    shortDescription?: string;
+    image: string;
+    isBlocked: boolean;
+    user?: User;
+}
+
+export interface User {
+    userName: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    role: string;
+    events?: Event[];
+}
